7fc384239a420e034ca553729c75e5ae9e012dcb
Brenda <Eelisarromero@outlook.es>
2019-04-11 16:54:33 -0500

    Tabla proveedores

---
+++ database/migrations/2019_04_10_000000_create_proveedores_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('telefono'); 
+            $table->string('fax');
+            $table->string('correo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
