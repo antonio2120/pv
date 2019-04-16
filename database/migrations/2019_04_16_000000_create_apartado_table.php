ff853903676810f3db7e92dc3caeaa42d881b729
Monica SH <monik98sh@gmail.com>
2019-04-16 16:51:12 -0500

    Tabla apartado

---
+++ database/migrations/2019_04_16_000000_create_apartado_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComprasTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('apartado', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('clientes_id');
+            $table->string('fecha_inicio');
+            $table->string('fecha_fin');
+            $table->string('anticipo');
+            $table->string('total');
+            $table->unsignedInteger('empleados_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('apartado');
+    }
+}
