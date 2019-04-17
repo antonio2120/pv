cf685290a9a9b763af191d0cf928286a3e33f193
Luis Cruz <lcruz@frontrunnertechnologies.net>
2019-04-17 08:54:39 -0500

    Vista de Inicio

--- resources/views/proveedores.blade.php
+++ resources/views/proveedores.blade.php
@@ -6,10 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title><?php echo $title ?></title>
+    <title>{{$title}}</title>
 </head>
 <body>
-    <h1><?php echo $title ?></h1>
+    <h1>{{$title}} </h1>
     <table border="1">
         <tr>
             <th>Nombre Proveedor</th>
