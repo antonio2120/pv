655a17315950b7370f1fe22f74570b8c2a9c5258
victorHernandez16 <maxer_16@hotmail.com>
2019-05-20 07:31:45 -0500

    Ventas: Metodo guardar y validación

--- resources/views/ventasNuevo.blade.php
+++ resources/views/ventasNuevo.blade.php
@@ -1,21 +1,25 @@
 @extends('layout_principal')
 @section('content')
     <h1>{{$title}}</h1>
-    <form id="FormularioVenta" action="regis_val.php" method="POST">
+    <form id="FormularioVenta"  method="POST">
   <div class="row">
     <div class="col">
-      <label for="exampleInputEmail1">ID Empleado en turno</label>
-      <input type="text" class="form-control" placeholder="ID Empleado" id="forid_empleado" name="id_empleado">
+      <label for="exampleInputEmail1">Nombre de Empleado en turno</label>
+      <select class="form-control" id="id_empleado" name="id_empleado">
+        @foreach($empleados as $empleado)
+          <option value="{{$empleado->id}}">{{$empleado->nombre}}</option>
+        @endforeach
+      </select>
     </div>
   </div>
   <div class="row">
   	<div class="col">
-      <label for="inputFecha">Fecha de la venta</label>
-    <input type="text" class="form-control" id="forFecha" placeholder="Fecha" name="fecha">
+      <label for="inputFecha">Fecha de la venta (AA-MM-DD)</label>
+    <input type="text" class="form-control" id="Fecha" placeholder="Fecha" name="fecha">
     </div>
     <div class="col">
       <label for="inputHora">Hora de la venta</label>
-      <input type="text" class="form-control" placeholder="Hora" id="forHora" name="hora">
+      <input type="text" class="form-control" placeholder="Hora" id="Hora" name="hora">
     </div>
     
   </div>
@@ -23,12 +27,12 @@
 
     <div class="form-group col-md-4">
       <label for="inputTotal">Total de la venta</label>
-      <input type="Total" class="form-control" id="forTotal" placeholder="Total" name="Total">
+      <input type="Total" class="form-control" id="Total" placeholder="Total" name="Total">
     </div>
   </div>
   <div class="form-group">
     <div class="form-check" >
-      <input class="form-check-input" type="checkbox" id="forTerminos" name="terminos">
+      <input class="form-check-input" type="checkbox" id="Terminos" name="terminos">
       <label class="form-check-label" for="gridCheck">
         Acepto que los datos son correctos
       </label>
@@ -54,7 +58,46 @@
              Total:{required: "El total es OBLIGATORIO",}
 
            }
-      })
+      });
+       $("#FormularioVenta").submit(function (event ) {
+         console.log('submit');
+         console.log('validate', $("#FormularioVenta").validate());
+         event.preventDefault();
+
+         if ($("#FormularioVenta").validate()) {
+           $.ajax({
+             url: 'ventasGuardar',
+             method: 'POST',
+             data: {
+               fecha: $("#Fecha").val(),
+               hora: $("#Hora").val(),
+               total: $("#Total").val(),
+               empleado_id: $("#id_empleado").val(),
+               _token: "{{ csrf_token() }}",
+             },
+             dataType: 'json',
+             beforeSend: function () {
+
+             },
+             success: function (response) {
+               console.log("response", response);
+               if (response.status == 'ok') {
+                 toastr["success"](response.mensaje);
+                 $("#productoForm").trigger("reset");
+               } else {
+                 toastr["error"](response.mensaje);
+               }
+             },
+             error: function () {
+               toastr["error"]("Error al realizar el registro");
+             },
+             complete: function () {
+
+             }
+
+           })
+         }
+       });
 
     </script>
 
