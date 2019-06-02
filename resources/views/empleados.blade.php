80047d5a586ad66b6f7ad54123d01d39a9ba033e
anaMiriam16 <myriamdetommo@gmail.com>
2019-06-02 17:06:16 -0500

    Arreglo botón PDF empleados

--- resources/views/empleados.blade.php
+++ resources/views/empleados.blade.php
@@ -8,8 +8,8 @@
     <div class="col-4">
     <div class="form-inline my-2 my-lg-0">
     <input class="form-control mr-sm-2" id="buscar" type="text" placeholder="Ingresar búsqueda" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" onclick="buscar()">Buscar</button>
-    <button class="btn btn-outline-primary my-2 my-sm-0" onclick="imprimir('{{isset($buscar) ? $buscar : null }}')" type="button"><i class="fas fa-file-pdf"></i></button>
+    <button class="btn btn-outline-success my-2 my-sm-0 mr-sm-2" onclick="buscar()">Buscar</button>
+    <button class="btn btn-outline-primary my-2 my-sm-2" onclick="imprimir('{{isset($buscar) ? $buscar : null }}')" type="button"><i class="fas fa-file-pdf"></i></button>
   </div>
 </div>
 </div>
